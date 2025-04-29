3c3258b4848ec698040200c2addc50a681987ba6
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-29 11:19:51 -0300

    Add Auth Request, Response and Controller.

---
+++ src/main/java/com/tecnologiaefinancas/authify/io/AuthRequest.java
@@ -0,0 +1,16 @@
+package com.tecnologiaefinancas.authify.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    private String email;
+    private String password;
+}
