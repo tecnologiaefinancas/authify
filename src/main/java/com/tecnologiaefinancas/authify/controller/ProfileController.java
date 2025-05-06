9ba8966ffa76f5a7cbb22b82d1ba30c30545eb44
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-05-06 15:30:56 -0300

    Create Email Service and Profile Controller. Endpoint working. E-mail received. We used Brevo to this service.

--- src/main/java/com/tecnologiaefinancas/authify/controller/ProfileController.java
+++ src/main/java/com/tecnologiaefinancas/authify/controller/ProfileController.java
@@ -2,6 +2,7 @@ package com.tecnologiaefinancas.authify.controller;
 
 import com.tecnologiaefinancas.authify.io.ProfileRequest;
 import com.tecnologiaefinancas.authify.io.ProfileResponse;
+import com.tecnologiaefinancas.authify.service.EmailService;
 import com.tecnologiaefinancas.authify.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
 
     private final ProfileService profileService;
+    private final EmailService emailService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public ProfileResponse register(@Valid @RequestBody ProfileRequest request){
         ProfileResponse response = profileService.createProfile(request);
-        //TODO: send welcome email
+        emailService.sendWelcomeEmail(response.getEmail(), response.getName());
         return response;
     }
 
---
+++ src/main/java/com/tecnologiaefinancas/authify/service/EmailService.java
@@ -0,0 +1,27 @@
+package com.tecnologiaefinancas.authify.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to Our Plataform");
+        message.setText("Hello " + name + ", \n\nThanks for registering with us!\n\n" +
+                "Regards,\nAuthify Team");
+        mailSender.send(message);
+    }
+}
